ccf861c95356a13ca5c067cdade67b5918034d4f
rattanakone <rattanakone@users.noreply.github.com>
2015-10-28 23:20:51 -0700

    angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jiffApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jiffApp
+ */
+angular.module('jiffApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
