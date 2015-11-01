5a654205c9252c96150cc3430d4fd31c22be102a
rattanakone <rattanakone@users.noreply.github.com>
2015-10-31 22:25:02 -0700

    added style and history

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,13 @@
 angular.module('jiffApp')
   .controller('MainCtrl', function ($scope, JiffVid, $sce) {
   	$scope.videos = JiffVid.query();
-  	//console.log($scope.videos.pageInfo.totalResults);
   	$scope.getRandomVideo = function () {
   		var randomIndex = Math.floor(Math.random()*50) +1;
-  		var embedCode = '<iframe ng-if="currentVideo" width="560" height="315" src="https://www.youtube.com/embed/' + $scope.videos.items[randomIndex].id.videoId + '" frameborder="0" allowfullscreen></iframe>';
+  		var embedCode = '<iframe ng-if="currentVideo" width="500" height="315" src="https://www.youtube.com/embed/' + $scope.videos.items[randomIndex].id.videoId + '" frameborder="0" allowfullscreen></iframe>';
   		$scope.currentVideo = $sce.trustAsHtml(embedCode);
-      //$scope.previousVideo = $sce.trustAsHtml(embedCode);
+      var createNew = document.createElement("li");
+      createNew.innerHTML = $sce.trustAsHtml(embedCode);
+      document.getElementById("previousVideo").appendChild(createNew);
   		console.log("set current video to: " + $scope.currentVideo);
 
   	};
